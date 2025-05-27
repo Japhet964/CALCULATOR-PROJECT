df4e636133706ad227e79e96dcb429bac2dbe2cd
Japhet964 <japhetandenyang55@gmail.com>
2025-05-27 07:32:27 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,5 @@
 # Simple Calculator
 
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-# operation = input("Enter the operation (+, -, *, /): ")
-
-# if operation == '+':
-#     result = num1 + num2
-#     print("Result:", result)
-# elif operation == '-':
-#     result = num1 - num2
-#     print("Result:", result)
-# elif operation == '*':
-#     result = num1 * num2
-#     print("Result:", result)
-# elif operation == '/':
-#     if num2 != 0:
-#         result = num1 / num2
-#         print("Result:", result)
-#     else:
-#         print("Error: Cannot divide by zero.")
-# else:
-#     print("Invalid operation. Please choose +, -, *, or /.")
-
-
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Enter the oparetion (+, *, -, /) :")
